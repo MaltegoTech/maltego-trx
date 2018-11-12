cea11cef3c12172005a8db494f98a92918581608
Maltego Staff <technical@paterva.com>
2018-11-12 12:18:55 +0200

    Add apache demo

---
+++ demo/apache/transforms/GreetPerson.py
@@ -0,0 +1,15 @@
+from maltego_trx.entities import Phrase
+
+from maltego_trx.transform import DiscoverableTransform
+
+
+class GreetPerson(DiscoverableTransform):
+    """
+    Returns a phrase greeting a person on the graph.
+    """
+
+    @classmethod
+    def create_entities(cls, request, response):
+        person_name = request.Value
+
+        response.addEntity(Phrase, "Hi %s, nice to meet you!" % person_name)
