fc4b856916687209988caf481d0da8ecacbba974
felixhertrampf <fe@maltego.com>
2021-06-23 19:18:56 +0200

    fix: added handler.py back

---
+++ maltego_trx/handler.py
@@ -0,0 +1,33 @@
+import uvicorn
+
+from maltego_trx import VERSION
+from .maltego import MaltegoRequest
+from .server import print_transforms, run_transform, get_exception_message, registry
+
+"""
+Receive commands run inside a project folder.
+"""
+
+
+def handle_run(name, args, app, port=8080, ssl_context=None, debug=False, ssl_keyfile=None, ssl_certfile=None):
+    if name == "__main__":
+        if len(args) >= 2:
+            command = args[1].lower()
+            if command == "runserver":
+                print("\n=== Maltego Transform Server: v%s ===\n" % VERSION)
+                print_transforms()
+                uvicorn.run(app, host="0.0.0.0", port=port, debug=debug, reload=debug, ssl_keyfile=ssl_keyfile, ssl_certfile=ssl_certfile)
+                # app.run(host="0.0.0.0", port=port, debug=debug, ssl_context=ssl_context)
+            elif command == "list":
+                print_transforms()
+            elif command == "local" and len(args) > 3:
+                transform_name = args[2].lower()
+                if transform_name in registry.transform_classes:
+                    client_msg = MaltegoRequest.from_local_args(args[3:])
+                    print(run_transform(transform_name, client_msg)[0])
+                else:
+                    print(get_exception_message(msg="Unable to find a transform matching '%s'." % transform_name))
+
+        else:
+            commands = ["list", "local", "runserver"]
+            print("Command not recognised. Available commands are:\r\n{0}".format("\r\n".join(commands)))
