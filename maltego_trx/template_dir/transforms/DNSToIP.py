820a11919d7e61a8c8f9773e528536ca4dad2959
Felix Hertrampf <fhe@maltego.com>
2021-12-16 13:33:20 +0100

    feat(starter): add type hints and registry configurations

--- maltego_trx/template_dir/transforms/DNSToIP.py
+++ maltego_trx/template_dir/transforms/DNSToIP.py
@@ -1,24 +1,25 @@
 import socket
-from maltego_trx.maltego import UIM_TYPES
-from maltego_trx.entities import IPAddress
 
+from extensions import registry
+from maltego_trx.entities import IPAddress
+from maltego_trx.maltego import UIM_TYPES, MaltegoMsg, MaltegoTransform
 from maltego_trx.transform import DiscoverableTransform
 
 
+@registry.register_transform(display_name="DNS to IP", input_entity="maltego.DNSName",
+                             description='Receive DNS name from the Client, and resolve to IP address.',
+                             output_entities=["maltego.IPv4Address"])
 class DNSToIP(DiscoverableTransform):
-    """
-    Receive DNS name from the client, and resolve to IP address.
-    """
 
     @classmethod
-    def create_entities(cls, request, response):
+    def create_entities(cls, request: MaltegoMsg, response: MaltegoTransform):
         dns_name = request.Value
 
         try:
             ip_address = socket.gethostbyname(dns_name)
             response.addEntity(IPAddress, ip_address)
         except socket.error as e:
-            response.addUIMessage("Error: " + str(e), UIM_TYPES["partial"])
+            response.addUIMessage(f"Error: {e}", UIM_TYPES["partial"])
 
         # Write the slider value as a UI message - just for fun
-        response.addUIMessage("Slider value is at: " + str(request.Slider))
+        response.addUIMessage(f"Slider value is at: {request.Slider}")
--- maltego_trx/template_dir/transforms/GreetPerson.py
+++ maltego_trx/template_dir/transforms/GreetPerson.py
@@ -1,15 +1,17 @@
+from extensions import registry
 from maltego_trx.entities import Phrase
+from maltego_trx.maltego import MaltegoMsg, MaltegoTransform
 
 from maltego_trx.transform import DiscoverableTransform
 
 
+@registry.register_transform(display_name="Greet Person", input_entity="maltego.Phrase",
+                             description='Returns a Phrase greeting a Person on the Graph.',
+                             output_entities=["maltego.Phrase"])
 class GreetPerson(DiscoverableTransform):
-    """
-    Returns a phrase greeting a person on the graph.
-    """
 
     @classmethod
-    def create_entities(cls, request, response):
+    def create_entities(cls, request: MaltegoMsg, response: MaltegoTransform):
         person_name = request.Value
 
-        response.addEntity(Phrase, "Hi %s, nice to meet you!" % person_name)
+        response.addEntity(Phrase, f"Hi {person_name}, nice to meet you!")
