2ce68547675a8315c16d9babe1202d649ac90351
felixhertrampf <fhe@maltego.com>
2022-06-08 16:56:28 +0200

    feat: add README as pypi description

--- setup.py
+++ setup.py
@@ -1,10 +1,15 @@
 from setuptools import setup
 from maltego_trx import VERSION
 
+with open("README.md", "r") as readme_md:
+    long_description = readme_md.read()
+
 setup(
     name='maltego-trx',
     version=VERSION,
     description='Python library used to develop Maltego transforms',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     url='https://github.com/paterva/maltego-trx/',
     author='Maltego Staff',
     author_email='support@maltego.com',
